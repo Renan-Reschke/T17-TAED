3b29e2ec61c256c9bd6cda99925dec591b730a08
Renan Reschke <renanreschke@hotmail.com>
2021-08-18 18:23:18 -0300

    classe TestHash

--- exercicio02/src/maua/SList.java
+++ exercicio02/src/maua/SList.java
@@ -8,7 +8,27 @@ public class SList {
         this.conteudo = conteudo;
         this.proximo = null;
     }
-    private void addLast(Integer key){
-        this.proximo = new SList(key);
+    public void InsereInicio(Integer key){
+        this.proximo = new SList(this.conteudo);
+        this.conteudo = key;
+    }
+
+    public Integer getConteudo(){
+        return this.conteudo;
+    }
+
+    public SList getProximo(){
+        return this.proximo;
+    }
+
+    static public String printLista(SList lista){
+        String texto = "";
+        SList atual = lista;
+        texto += atual.getConteudo().toString();
+        while(atual.proximo != null){
+            atual = atual.proximo;
+            texto += ", " + atual.getConteudo().toString();
+        }
+        return texto;
     }
 }
---
+++ exercicio02/src/maua/TestHash.java
@@ -0,0 +1,46 @@
+package maua;
+
+import static maua.SList.printLista;
+
+public class TestHash {
+    public static void main(String[] args) {
+
+        Integer[] tabKeys = new Integer[20];
+        for(int i=1; i<tabKeys.length; i++){
+            tabKeys[i] = i;
+        }
+
+        Integer hashCode, chave;
+        SList[] tabHash = new SList[10];
+
+        for(int i=1; i<tabKeys.length; i++){
+            chave = tabKeys[i];
+            hashCode = hash(chave);
+            System.out.println("Chave = " + chave +
+                    " mapeada para hascode = " + hashCode);
+            if (tabHash[hashCode] == null )
+                tabHash[hashCode] = new SList(tabKeys[i]);
+            else {
+                tabHash[hashCode].InsereInicio(tabKeys[i]);
+                System.out.println("** Colisao no slot da Tabela Hash ** ");
+                System.out.println("Chave " + tabKeys[i] +
+                        " adicionada na lista ligada ...\n ");
+            }
+        }
+        System.out.println(printLista(tabHash[2]));
+        System.out.println("\nTabela HASH:");
+        System.out.println("--------------------------");
+        for(int i=0; i<tabHash.length; i++){
+            if (tabHash[i] == null)
+                System.out.println("Slot " + i + " ---> Valor nulo");
+            else
+                System.out.println("Slot" + i + " ---> " + printLista(tabHash[i]));
+        }
+    }
+
+    public static Integer hash(Integer key) {
+        return (key % 10);
+    }
+
+
+}
