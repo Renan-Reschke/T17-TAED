40c2f48ef7edc216b398b4f539a84b41b5cd4b5e
Renan Reschke <renanreschke@hotmail.com>
2021-08-18 19:29:08 -0300

    .

--- exercicio02/src/maua/SList.java
+++ exercicio02/src/maua/SList.java
@@ -1,33 +1,67 @@
 package maua;
 
 public class SList {
-    private Integer conteudo;
-    private SList proximo;
 
-    SList(Integer conteudo){
-        this.conteudo = conteudo;
-        this.proximo = null;
+    private static class No{
+        Integer conteudo;
+        No proximo;
+
+        public No(Integer conteudo){
+            this.conteudo = conteudo;
+            this.proximo = null;
+        }
+
+        public No getProximo(){
+            return this.proximo;
+        }
+
+        public Integer getConteudo(){
+            return this.conteudo;
+        }
     }
+
+    private Integer tamanho = 0;
+    private No head = null;
+
+    SList(){
+    }
+
+    public int tamanho(){
+        return this.tamanho;
+    }
+
+    public boolean isEmpty() {
+        return tamanho == 0;
+    }
+
     public void InsereInicio(Integer key){
-        this.proximo = new SList(this.conteudo);
-        this.conteudo = key;
+        if(this.tamanho == 0){
+            No novoNo = new No(key);
+            this.tamanho++;
+            this.head = novoNo;
+        } else{
+            No novoNo = new No(key);
+            novoNo.proximo = this.head;
+            this.head = novoNo;
+            this.tamanho++;
+        }
+
+
     }
 
-    public Integer getConteudo(){
-        return this.conteudo;
+    public No getHead(){
+        return this.head;
     }
 
-    public SList getProximo(){
-        return this.proximo;
-    }
-
-    static public String printLista(SList lista){
+    public String printLista(){
         String texto = "";
-        SList atual = lista;
-        texto += atual.getConteudo().toString();
-        while(atual.proximo != null){
-            atual = atual.proximo;
-            texto += ", " + atual.getConteudo().toString();
+        if(!this.isEmpty()){
+            No atual = this.head;
+            texto += atual.getConteudo().toString();
+            while(atual.proximo != null){
+                atual = atual.proximo;
+                texto += ", " + atual.getConteudo().toString();
+            }
         }
         return texto;
     }
--- exercicio02/src/maua/TestHash.java
+++ exercicio02/src/maua/TestHash.java
@@ -1,11 +1,9 @@
 package maua;
 
-import static maua.SList.printLista;
-
 public class TestHash {
     public static void main(String[] args) {
 
-        Integer[] tabKeys = new Integer[20];
+        Integer[] tabKeys = new Integer[100];
         for(int i=1; i<tabKeys.length; i++){
             tabKeys[i] = i;
         }
@@ -18,8 +16,10 @@ public class TestHash {
             hashCode = hash(chave);
             System.out.println("Chave = " + chave +
                     " mapeada para hascode = " + hashCode);
-            if (tabHash[hashCode] == null )
-                tabHash[hashCode] = new SList(tabKeys[i]);
+            if (tabHash[hashCode] == null ) {
+                tabHash[hashCode] = new SList();
+                tabHash[hashCode].InsereInicio(tabKeys[i]);
+            }
             else {
                 tabHash[hashCode].InsereInicio(tabKeys[i]);
                 System.out.println("** Colisao no slot da Tabela Hash ** ");
@@ -27,14 +27,14 @@ public class TestHash {
                         " adicionada na lista ligada ...\n ");
             }
         }
-        System.out.println(printLista(tabHash[2]));
+
         System.out.println("\nTabela HASH:");
         System.out.println("--------------------------");
         for(int i=0; i<tabHash.length; i++){
             if (tabHash[i] == null)
                 System.out.println("Slot " + i + " ---> Valor nulo");
             else
-                System.out.println("Slot" + i + " ---> " + printLista(tabHash[i]));
+                System.out.println("Slot" + i + " ---> " + tabHash[i].printLista());
         }
     }
 
